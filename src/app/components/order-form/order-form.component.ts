105e6397a820b1cffab93d82c3452a62c4791012
405193-OLIVA <405193@tecnicatura.frc.utn.edu.ar>
2024-11-08 00:49:03 -0300

    index config

--- src/app/components/order-form/order-form.component.ts
+++ src/app/components/order-form/order-form.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './order-form.component.html',
   styleUrl: './order-form.component.css'
 })
 export class OrderFormComponent {
+  orderForm: FormGroup = new FormGroup({
+    customerName: new FormControl(),
+    email: new FormControl(),
+    products: new FormArray([])
+  });
 
+  get products(){
+    return this.orderForm.controls['products'] as FormArray;
+  }
+
+  addProd(){
+    const productForm = new FormGroup({
+      
+    })
+  }
 }
